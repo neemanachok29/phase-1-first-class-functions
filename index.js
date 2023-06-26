bd2f6064793c4ca5568f0195d2a3cd170027398e
Neema <neemanachok29@gmail.com>
2023-06-26 14:49:52 +0300

    submit

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback){
+    callback()
+}
+
+function returnsANamedFunction(){
+    return function hello() {
+        console.log('hello, there.')
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return  () => {
+        console.log('Hello, there.')
+    }
+}
